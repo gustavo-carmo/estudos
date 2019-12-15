40ee4d575f2267d2a2797f6b2cdc8adc981e82d3
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2019-12-15 13:13:34 -0300

    Start course ECMAScript 6

---
+++ ECMAScript/Skylab/curso-es6/main.js
@@ -0,0 +1,13 @@
+class TodoList {
+
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo() {
+    this.todos.push("Novo todo");
+    console.log(this.todos);
+  }
+}
+
+const minhaLista = new TodoList();
