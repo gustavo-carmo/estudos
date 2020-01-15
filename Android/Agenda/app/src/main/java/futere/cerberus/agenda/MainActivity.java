93600743783c1be09f8df41fde5b543f9173ec43
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2020-01-15 11:26:17 -0300

    Adding Android Project + Docker Readme to installation

---
+++ Android/Agenda/app/src/main/java/futere/cerberus/agenda/MainActivity.java
@@ -0,0 +1,18 @@
+package futere.cerberus.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {        
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
